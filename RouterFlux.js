bf28a8b337308d10a0f1766dd67432affa3dbc2d
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-24 20:09:30 +0530

    Update

--- RouterFlux.js
+++ RouterFlux.js
@@ -4,13 +4,20 @@ import Login from './src/components/Login';
 import DashBoard from './src/components/DashBoard';
 import ImageView from './src/components/ImageView'
 
+// Created reusable component router flux
 class RouterFlux extends Component  {
   render() {
     return (
+        // Routing between screens using key
         <Router>
-          <Scene key="root" >
+         <Scene key="root" >
+              // login as Initial component
               <Scene key="login" component={Login}  initial  hideNavBar={true} />
+
+              // Route for dashboard component
               <Scene key="dashboard" component={DashBoard} hideNavBar={true}  />
+
+              //Route for imageView component
               <Scene key="imageView" component={ImageView} hideNavBar={false} />
           </Scene>
        </Router>
