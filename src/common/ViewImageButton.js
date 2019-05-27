d3f97cc1c29bc9b293360b0f260d3d4b02544386
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 16:28:02 +0530

    Update ViewImageButton.js

--- src/common/ViewImageButton.js
+++ src/common/ViewImageButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import CommonStyle from './CommonStyle';
+import CommonStyle from './CommonStyle'; // Importing common styling component
+
+// Created reusable component full ViewImageButton 
 
 const ViewImageButton = ({ onPress,children }) => {
   return (
       <TouchableOpacity onPress={onPress} style={CommonStyle.viewImagebuttonStyle}>
         <Text style={CommonStyle.viewImageButtontextStyle}> 
-            {children}
+            {children}    // Fetching data from parent component
         </Text>
       </TouchableOpacity>
   );
