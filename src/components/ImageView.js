e15c4e666fdd42de58de403ca9afbf793297b18c
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 15:55:17 +0530

    Update ImageView.js

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { Button, Card, CardSection } from '../common';
-import ComponentStyle from './ComponentStyle';
+import { Button, Card, CardSection } from '../common'; // Importing usable components from common folder
+import ComponentStyle from './ComponentStyle'; // Importing common styling component
 import Lightbox from 'react-native-lightbox';
 
+// Created reusable ImageView component
+
 const ImageView = ({ data }) => {
-      const{ id, author, download_url, url, width, height} = data;
+      const{ id, author, download_url, url, width, height} = data; // Fetching data from parent component 
   return (
     <ScrollView>
       <Card>
+      // Rendring image with zoom functionality 
           <Lightbox>
             <CardSection >
-              <Image style={ComponentStyle.fullImageStyle} source={{ uri: download_url }} />
+               <Image style={ComponentStyle.fullImageStyle} source={{ uri: download_url }} /> 
             </CardSection>
           </Lightbox>
           <CardSection>
+        
+       // Rendring full image information
             <View style={ComponentStyle.imageViewContentStyle}>
               <Text style={ComponentStyle.idTextStyle}>Id: {id}</Text>
               <Text style={ComponentStyle.authoTextStyle}>Author: {author}</Text>
