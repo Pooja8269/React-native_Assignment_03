49be6553642ca098f6f5c1657f576c0622e48dd8
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 15:26:58 +0530

    Update AlbumList.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,11 @@
 import React,{Component} from 'react';
 import { ScrollView, ActivityIndicator, View} from 'react-native';
 import AlbumDetail from './AlbumDetail';
-import ComponentStyle from './ComponentStyle'
+import ComponentStyle from './ComponentStyle' // Importing common styling component
 
+// 
 class AlbumList extends Component{
-state = { data:[], loading: true };
+state = { data:[], loading: true }; // Initializing state to store data  
 
               /** Another way to fetch API through npm plugin */
              // import axios from 'axios'; first  need to import this plugin
@@ -13,6 +14,7 @@ state = { data:[], loading: true };
              //   .then(response => this.setState({data: response.data}));
              // }
 
+// fetching API data using traditional method 
   componentWillMount(){
     fetch('https://picsum.photos/v2/list?page=2&limit=100').then(response => response.json())
       .then((responseJson)=> {
@@ -20,22 +22,27 @@ state = { data:[], loading: true };
       }).catch(error=>console.log(error))
   }
 
+// renderAlbums method to render data into AlbumDetail component
   renderAlbums(){
     var size = 15;
     var data = this.state.data.slice(0,size);
     return data.map(data => <AlbumDetail key={data.id} data={data} />);
   }
 
+// Rendering the data                    
   render(){
+      
+   // Data fetched loading condition 
     if(this.state.loading){
       return( 
         <View style={ComponentStyle.loader}> 
            <ActivityIndicator size="large" color='#03255b'/>
         </View>
     )}
+  
     return(
       <ScrollView>
-        {this.renderAlbums()}
+        {this.renderAlbums()} // renderAlbums function calling
       </ScrollView> 
     );
   } 
