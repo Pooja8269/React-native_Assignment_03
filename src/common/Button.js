0b139fe7a3d6e560ddb1857585ece1f54a719d4d
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 16:21:25 +0530

    Update Button.js

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import CommonStyle from './CommonStyle'
+import CommonStyle from './CommonStyle' // importing comman styling component
 
-const Button = ({ onPress,children }) => {
+// Created reusable component button 
+const Button = ({ onPress,children }) => { 
     return (
       <TouchableOpacity onPress={onPress} style={CommonStyle.buttonStyle}>
-         <Text style={CommonStyle.buttontextStyle}>  {children} </Text>
+         <Text style={CommonStyle.buttontextStyle}>  {children} </Text> 
       </TouchableOpacity>
     );
 };
