4aa9a69dda7f9e433481f1d7d464ad35b7db88a0
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 14:58:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import RouterFlux from './RouterFlux';
 
+// Application root component app
 export default class App extends Component {
   render() {
     return (
-      <RouterFlux />
+      <RouterFlux /> // using RouterFlux component for routing
     );
   }
 }
