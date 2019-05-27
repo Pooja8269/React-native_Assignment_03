303f738c27d78de4dcb13b4c69ce8d0802052c8a
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 16:23:19 +0530

    Update Card.js

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-import CommonStyle from './CommonStyle'
+import CommonStyle from './CommonStyle' // Importing common styling component
 
+// Created reusable component Card 
 const Card = (props) => {
   return (
     <View style={CommonStyle.cardContainerStyle}>
-        {props.children}
+        {props.children}  // Fetching data from parent component using props
     </View>
   );
 };
