fb3d0ed97d782ca0bc2e72d3a9d2421b8e044aac
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 16:24:20 +0530

    Update CardSection.js

--- src/common/CardSection.js
+++ src/common/CardSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-import CommonStyle from './CommonStyle'
+import CommonStyle from './CommonStyle' // Importing common styling component
 
+// Created reusable component CardSection 
 const CardSection = (props) => {
   return(
       <View style={CommonStyle.cardSectionStyle}>
-          {props.children}
+          {props.children}  // Fetching data from parent component using props
       </View>
   );
 };
