6f230a32914043c450cb940498c34ccae0a175a4
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 15:49:57 +0530

    Update AlbumDetail.js

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,24 +4,32 @@ import { ViewImageButton, Card, CardSection, Button } from '../common';
 import { Actions } from 'react-native-router-flux';
 import ComponentStyle from './ComponentStyle'
 
-    const onButtonPress = ({data}) => {
-      Actions.imageView({data});
+// Full image dedail function 
+    const onImageView = ({data}) => {
+      Actions.imageView({data}); //Navigate data to <ImageView> component
     }
 
+// Created reusable component AlbumDetail 
 const AlbumDetail = ({data}) => {
-    const{ id, author, download_url,url} = data
+    const{ id, author, download_url,url} = data // Fetching parsed data
   return (
+       // Rendering all data 
     <Card>
         <CardSection>
             <View style={ComponentStyle.thumbnailContainerStyle}>
-              <Image style={ComponentStyle.thumbnailStyle} source={{ uri: download_url }} />
+      // Rendering image data 
+              <Image style={ComponentStyle.thumbnailStyle} source={{ uri: download_url }} /> //
             </View>
+      
+        // Rendering some of image details 
             <View style={ComponentStyle.imageContentStyle}>
               <View style={ComponentStyle.imageContentStyle}>
                 <Text style={ComponentStyle.imageIdStyle}>Id: {id}</Text>
                 <Text style={ComponentStyle.authorNameStyle}>Author: {author}</Text>
               </View>
-              <ViewImageButton onPress={() => onButtonPress({data})}> View Image Details </ViewImageButton>
+      
+       // onImageView function calling 
+              <ViewImageButton onPress={() => onImageView({data})}> View Image Details </ViewImageButton>
             </View>
         </CardSection>
     </Card>
