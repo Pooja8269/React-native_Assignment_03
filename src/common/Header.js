62d2fe43a48b631518ac61bdc9e84d8699e90e42
Pooja8269 <42864797+Pooja8269@users.noreply.github.com>
2019-05-27 16:25:25 +0530

    Update Header.js

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import CommonStyle from './CommonStyle'
+import CommonStyle from './CommonStyle' // Importing common styling component
 
+// Created reusable component Header
 const Header = (props) => {
  return (
     <View style={CommonStyle.headerViewStyle}>
+  
+  // Fetching headerText from parent component using props
         <Text style={CommonStyle.headerTextStyle}> {props.headerText} </Text> 
     </View>
   );
